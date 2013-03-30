5efac74000c3a79609df346a11f0d2e7fc7dce32
damian87x <borekdamian@yahoo.pl>
2013-03-30 19:06:30 +0100

    Add Pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def home
+    # variable for title definition in helper.
+    @title = "home"
+  end
+
+  def about
+    @title = "about"
+  end
+end
