59bb4b79ccd1845454b2301abca4918fa105b015
damian87x <borekdamian@yahoo.pl>
2013-04-09 19:48:24 +0200

    Add Image Upl. via URL

--- spec/models/motivator_spec.rb
+++ spec/models/motivator_spec.rb
@@ -2,10 +2,16 @@
 #
 # Table name: motivators
 #
-#  id          :integer          not null, primary key
-#  description :string(255)
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
+#  id                 :integer          not null, primary key
+#  description        :string(255)
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  user_id            :integer
+#  image_file_name    :string(255)
+#  image_content_type :string(255)
+#  image_file_size    :integer
+#  image_updated_at   :datetime
+#  image_remote_url   :string(255)
 #
 
 require 'spec_helper'
