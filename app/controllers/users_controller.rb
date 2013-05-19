c8d570b1150d2f4a182f263460961bb5bb3f4eb1
damian87x <borekdamian@yahoo.pl>
2013-05-19 21:34:54 +0100

    Change Profile User to show only his motivators

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @motivators = Motivator.where(approved: true).order("created_at desc").page(params[:page]).per_page(15)
+    @motivators = @user.motivators.where(approved: true).order("created_at desc").page(params[:page]).per_page(15)
   end
 end
+
+
