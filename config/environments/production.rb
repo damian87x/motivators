09678c974f572e79a80acf67224079ae5100f0ce
damian87x <borekdamian@yahoo.pl>
2013-06-04 15:59:49 +0100

    Configure mailer for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Motivators::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   #In production, :host should be set to the actual host of your application.
-  config.action_mailer.default_url_options = { :host => 'stark-gorge-5149.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'stark-gorge-5149.heroku.com' }
 
   #Configuring Amazon S3 for paperclip file upload
   config.paperclip_defaults = {
